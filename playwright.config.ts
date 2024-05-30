3739cead4a7a9c4f28b7df3134fb4ae2a00edf53
Omar Shalaby <omar@arabot.io>
2024-05-30 17:19:19 +0300

    resolves sonarlint issue

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,8 +5,6 @@ require('dotenv').config();
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// require('dotenv').config();
-
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
